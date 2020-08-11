e1e8865ee3fdf009b5c587dccfd0caf9a2f31653
Oscar Wales <oscarjwales@gmail.com>
2020-08-11 15:03:38 +0100

    Completed challenge on testing model methods

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    public static function haveWeBananas($number)
+    {
+        if ($number === 0) {
+            return "No we have no bananas";
+        }
+
+        if (($number === 1) || ($number === -1)) {
+            return "Yes we have {$number} banana";
+        }
+        return "Yes we have {$number} bananas";
+    }   
 }
