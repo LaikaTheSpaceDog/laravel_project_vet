0bdf2f3d54adc1c3ca6a19f23697c84ccc9bcadc
Oscar Wales <oscarjwales@gmail.com>
2020-08-11 12:00:24 +0100

    Completed non-tricksy laravel migrations challenges to create and add columns to owners table

---
+++ database/migrations/2020_08_11_103645_add_columns_to_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->string("address_1", 200)->default(0);
+            $table->string("address_2", 200)->nullable($value = true);
+            $table->string("town", 50)->default(0);
+            $table->string("postcode", 8)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->dropColumn("address_1");
+            $table->dropColumn("address_2");
+            $table->dropColumn("town");
+            $table->dropColumn("postcode");
+        });
+    }
+}
