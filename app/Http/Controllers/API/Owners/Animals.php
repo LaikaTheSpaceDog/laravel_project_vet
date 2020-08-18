bc1d2d15154293fbfce6989499b21dfec5b82cbe
Oscar Wales <oscarjwales@gmail.com>
2020-08-18 12:57:37 +0100

    Completed basic API challenges

---
+++ app/Http/Controllers/API/Owners/Animals.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API\Owners;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Owner;
+use App\Animal;
+
+class Animals extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Owner $owner)
+    {
+        return $owner->animals;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Owner $owner)
+    {
+        // $data = $request->all();
+        // if ($request->owner_id === $owner->id) {
+        //     return Animal::create($data);
+        // } else {
+        //     return "Inocrrect owner ID";
+        // }
+        $animal = new Animal($request->all());
+        $owner->animals()->save($animal);
+        return $animal;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Owners;
+use App\Http\Controllers\API\Animals;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(["prefix" => "owners"], function () {
+    Route::get("", [Owners::class, "index"]);
+    Route::post("", [Owners::class, "store"]);
+    Route::group(["prefix" => "{owner}"], function () {
+        Route::get("", [Owners::class, "show"]);
+        Route::put("", [Owners::class, "update"]);
+        Route::delete("", [Owners::class, "destroy"]);
+    });
+});
+
+Route::group(["prefix" => "animals"], function () {
+    Route::get("", [Animals::class, "index"]);
+    Route::group(["prefix" => "{animal}"], function () {
+        Route::get("", [Animals::class, "show"]);
+        Route::put("", [Animals::class, "update"]);
+        Route::delete("", [Animals::class, "destroy"]);
+    });
+});
+
+Route::group(["prefix" => "owners/{owner}/animals"], function () {
+    Route::get("", [Owners\Animals::class, "index"]);
+    Route::post("", [Owners\Animals::class, "store"]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
