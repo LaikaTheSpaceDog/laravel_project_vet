778eac80bd9444647a00ef140d93cfe2bee39640
Oscar Wales <oscarjwales@gmail.com>
2020-09-29 14:53:28 +0100

    Changed welcome message to jumbotron, reformatted footer so same width as header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,7 @@
-<footer class="container navbar navbar-expand-lg navabr-light bg-light">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">Copyright blah blah</li>
-    </ul>
+<footer class="container">
+    <section class="navbar navbar-expand-lg navabr-light bg-light">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">Copyright Oscar Wales 2020</li>
+        </ul>
+    </section>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,7 @@
 }}@endsection
 
 @section("content")
+<div class="jumbotron">
     <h3 class="display-3 text-center">{{ "{$greeting}, welcome to our pet owner database" }}</h3>
+</div>
 @endsection
